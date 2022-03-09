032796ad32a526a71e531ddd95af389330fddd41
Edeki Okoh <Okohedeki@gmail.com>
2022-03-08 23:22:24 -0700

    Checkin helper function

---
+++ helper/helper.go
@@ -0,0 +1,19 @@
+package helper
+
+func split(tosplit string, sep rune) []string {
+	var fields []string
+
+	last := 0
+	for i, c := range tosplit {
+		if c == sep {
+			// Found the separator, append a slice
+			fields = append(fields, string(tosplit[last:i]))
+			last = i + 1
+		}
+	}
+
+	// Don't forget the last field
+	fields = append(fields, string(tosplit[last:]))
+
+	return fields
+}
